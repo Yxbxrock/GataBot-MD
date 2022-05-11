7e717819c0e5a015270357bbe77e5290b0201797
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-11 17:13:22 -0500

    Update regist.js

--- plugins/regist.js
+++ plugins/regist.js
@@ -5,7 +5,7 @@ let handler = async function (m, { text, usedPrefix }) {
   if (user.registered === true) throw `╰⊱❕⊱ *𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊́𝙉 | 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝙏𝙄𝙊𝙉* ⊱❕⊱╮\n\nYa estás registrado(a).\n*_¿Quieres volver a registrarte?_*\n${usedPrefix}unreg *SN|NÚMERO DE SERIE*\n\nYou are already registered.\n*_Do you want to re-register?_*\n${usedPrefix}unreg *SN|SERIAL NUMBER*`
   if (!Reg.test(text)) throw `╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮\n\nFormato incorrecto\n*Ejemplo:*\n*${usedPrefix}registrar nombre.edad*\n\nIncorrect format\n*Example:*\n*${usedPrefix}daftar name.age*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*El nombre no puede estar vacío.*\nThe name cannot be empty.'
+  if (!name) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*El nombre no puede estar vacío.*\n*The name cannot be empty.*'
   if (!age) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*La edad no puede estar vacía.*\nAge cannot be empty.'
   age = parseInt(age)
   if (age > 120) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*Mucha edad 😹*\n*Very old* 😹'
@@ -28,6 +28,7 @@ ${sn}
 }
 handler.help = ['registrar | register | reg'].map(v => v + ' *nombre.edad*')
 handler.tags = ['xp']
+handler.exp = 7
 
 handler.command = /^(registrar|registar|daftar|reg(ister)?)$/i
 
