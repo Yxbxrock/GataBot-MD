a60efd230fa1d0606b6a7ac9ddb960701e6febe7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-11 14:12:19 -0500

    Update regist.js

--- plugins/regist.js
+++ plugins/regist.js
@@ -3,13 +3,13 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.db.data.users[m.sender]
   if (user.registered === true) throw `Ya estás registrado(a).\n_¿Quieres volver a registrarte?_ ${usedPrefix}unreg *SN|NÚMERO DE SERIE*`
-  if (!Reg.test(text)) throw `╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮\n\nFormato incorrecto\n*${usedPrefix}daftar nombre.edad*`
+  if (!Reg.test(text)) throw `╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮\n\nFormato incorrecto\n*${usedPrefix}daftar nombre.edad*\n`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*El nombre no puede estar vacío.*\n'
+  if (!age) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*La edad no puede estar vacía.*\n'
   age = parseInt(age)
-  if (age > 120) throw 'Umur terlalu tua 😂'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  if (age > 120) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*Mucha edad 😹*\n'
+  if (age < 10) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*Es muy poca edad 🙀*\n'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
@@ -26,9 +26,9 @@ Serial Number:
 ${sn}
 `.trim())
 }
-handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+handler.help = ['registrar | register | reg'].map(v => v + ' nombre.edad')
 handler.tags = ['xp']
 
-handler.command = /^(daftar|reg(ister)?)$/i
+handler.command = /^(registrar|registar|daftar|reg(ister)?)$/i
 
 module.exports = handler
