1a08dd6d9f67b1e6e945f3ceb45258edd05ac781
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-12 12:35:11 -0500

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -23,8 +23,8 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
             body: 'Now Playing...',
             description: 'Now Playing...',
             mediaType: 2,
-          thumbnail: await (await fetch('https://telegra.ph/file/76f08bd0aeb06c3e0c0d5.jpg')).buffer(),
-         mediaUrl: `https://youtube.com/watch?v=uIedYGN3NQQ`
+          thumbnail: await (await fetch('https://i.imgur.com/WwxBm8o.jpeg')).buffer(),
+         mediaUrl: `https://youtu.be/85xI8WFMIUY`
         }
      }
   })
