0793be1eb73f9dc495ec5e487fb7e321e86eefc6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-13 20:21:02 -0500

    Update nsfwneko.js

--- plugins/nsfwneko.js
+++ plugins/nsfwneko.js
@@ -8,10 +8,9 @@ handler.command = /^(nsfwneko)$/i
 module.exports = handler
 */
 
-     let handler  = async (m, { conn, usedPrefix, command }) => {
+    let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/jeni?apikey=APIKEY`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '💫 *Como las estrellas sobre las nubes*', 'Gata Dios', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, '💫 *Como las estrellas sobre las nubes*', 'Gata Dios', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(jeni|jenni|jennie|Jeni|Jenni)$/i
 module.exports = handler
