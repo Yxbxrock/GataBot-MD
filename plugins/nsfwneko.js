f8161f81c08312ba4fa1b8eed811c060d9d3211f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-13 20:17:44 -0500

    Update nsfwneko.js

--- plugins/nsfwneko.js
+++ plugins/nsfwneko.js
@@ -8,51 +8,27 @@ handler.command = /^(nsfwneko)$/i
 module.exports = handler
 */
 
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+//created by Scooppt
 let fetch = require('node-fetch')
-let handler = async (m, { conn, usedPrefix, command }) => {
-  var {age} = db.data.users[m.sender]
-  if (age <17) throw conn.reply(m.chat, 'Lu masih di bawah umur jangan dulu deh', m) 
-  let res = await fetch('https://api.waifu.pics/nsfw/neko')
-  if (!res.ok) throw conn.reply(m.chat, eror, m) 
-  let json = await res.json()
-  if (!json.url) throw conn.reply(m.chat, eror, m)
-  let message = await prepareWAMessageMedia({ image: await(await fetch(json.url)).buffer()}, { upload: conn.waUploadToServer })
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-      templateMessage: {
-            hydratedTemplate: {
-                imageMessage: message.imageMessage,
-                hydratedContentText: `Jangan sagne banh!`,
-                hydratedFooterText: wm,
-                hydratedButtons: [{
-                  index: 0,
-                   urlButton: {
-                        displayText: `🖼 Url Image`,
-                        url: `${json.url}`
-                    }
-                }, {
-                   quickReplyButton: {
-                        displayText: `🔞 Get Again`,
-                        id: `${usedPrefix}${command}`
-                    },
-                    selectedIndex: 1
-                }]
-            }
-        }
-    }), { userJid: m.participant || m.key.remoteJid, quoted: m });
-    return await conn.relayMessage(
-        m.key.remoteJid,
-        template.message,
-        { messageId: template.key.id }
-    )
-  //conn.sendFile(m.chat, json.url, '', 'sange kok ama kucing', m, 0, { thumbnail: Buffer.alloc(0) })
-}
-handler.help = ['nsfwneko']
-handler.tags = ['nsfw']
-handler.command = /^(nsfwneko|nekonsfw)$/i
 
-handler.private = true
-handler.limit = true
-handler.register = true
+let handler  = async (m, { conn, text }) => {
+  try {
+     let res = await fetch('https://cataas.com/cat')
+     let img = await res.buffer()
+     let caption = `
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈
+`.trim()
+    conn.sendFile(m.chat, img, 'cat.jpg', caption, m)
+   } catch (e) {
+        console.log(e)
+        throw '_*Erro!*_'
+    }
+}
+
+handler.help = ['cat']
+handler.tags = ['random']
+handler.command = /^cat|gato|gata|Cat|Gato|Gata$/i
+
+handler.fail = null
 
 module.exports = handler
