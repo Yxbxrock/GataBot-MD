33c1649b8207b621891d65accc6e86165c534d31
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-05-30 23:00:33 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -706,9 +706,9 @@ module.exports = {
                             pp = await this.profilePictureUrl(user, 'image')
                         } catch (e) {
                         } finally {
-                            text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
-                                (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang 1'))
-                                this.sendButtonLoc(id, await(await fetch(pp)).buffer(), text, '', "ok", "huuu", null)
+                            text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'selamat datang').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
+                                (chat.sBye || this.bye || conn.bye || 'selamat tinggal!'))
+                                this.sendButtonLoc(id, await(await fetch(pp)).buffer(), text, '', "Y", "huuu", null)
                                 }
                     }
                 }
