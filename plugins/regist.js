255af13f26aa84b9026e0fc46544f14ce0bfe474
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-05-31 16:20:23 +0700

    Update regist.js

--- plugins/regist.js
+++ plugins/regist.js
@@ -5,24 +5,24 @@ let handler = async function (m, { text, usedPrefix }) {
   if (user.registered === true) throw `╰⊱❕⊱ *𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝘾𝙄𝙊́𝙉 | 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝙏𝙄𝙊𝙉* ⊱❕⊱╮\n\nYa estás registrado(a).\n*_¿Quieres volver a registrarte?_*\n${usedPrefix}unreg *SN|NÚMERO DE SERIE*\n\nYou are already registered.\n*_Do you want to re-register?_*\n${usedPrefix}unreg *SN|SERIAL NUMBER*`
   if (!Reg.test(text)) throw `╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮\n\nFormato incorrecto\n*Ejemplo:*\n*${usedPrefix}registrar nombre.edad*\n\nIncorrect format\n*Example:*\n*${usedPrefix}daftar name.age*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*El nombre no puede estar vacío.*\n*The name cannot be empty.*'
-  if (!age) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*La edad no puede estar vacía.*\nAge cannot be empty.'
+  if (!name) throw '*The name cannot be empty.*'
+  if (!age) throw 'Age cannot be empty.'
   age = parseInt(age)
-  if (age > 120) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*Mucha edad 😹*\n*Very old* 😹'
-  if (age < 10) throw '╰⊱❌⊱ *𝙁𝘼𝙇𝙇𝙊́ | 𝙀𝙍𝙍𝙊𝙍* ⊱❌⊱╮\n\n*Es muy poca edad 🙀*\n*It is very young* 🙀'
+  if (age > 120) throw '😹*\n*Very old* 😹'
+  if (age < 10) throw ' 🙀*\n*It is very young* 🙀'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  m.reply(conn.sendBut(m.chat, `╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎𝙎* ⊱💚⊱╮
+  m.reply(conn.sendBut(m.chat, `╰⊱💚⊱ *REGISTERED* ⊱💚⊱╮
 
-╭━━━[ *USUARIO(A) | USER* ]━━━━⬣
-│ *Nombre | Name:* ${name}
-│ *Edad | Age:* ${age}  
+╭━━━[ *USER* ]━━━━⬣
+│ *Name:* ${name}
+│ *Age:* ${age}  
 ╰━━━━━━━━━━━━━━━━━━⬣
-*Número de serie | Serial number*
-${sn}`, gt , `☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩`, '.menu',  m))
+*Serial number*
+${sn}`, gt , `☘ Menu`, '.menu',  m))
 }
 handler.help = ['registrar | register | reg'].map(v => v + ' *nombre.edad*')
 handler.tags = ['xp']
